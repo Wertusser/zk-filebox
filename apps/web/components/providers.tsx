3452891e4564a463270df9d655cd5a8515e0a92b
Georgii Molchanov <wertusser@gmail.com>
2025-10-26 09:24:41 +0000

    fix: wallet connect indexeddb bug

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -3,21 +3,59 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import {
-  getDefaultConfig,
   RainbowKitProvider,
   darkTheme,
+  connectorsForWallets,
 } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
+import { createConfig, http, WagmiProvider } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import {
+  rainbowWallet,
+  walletConnectWallet,
+  rabbyWallet,
+  metaMaskWallet,
+  injectedWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
-const config = getDefaultConfig({
-  appName: "zkFilebox",
-  projectId: "YOUR_PROJECT_ID",
+const wagmiConnectors = () => {
+  // Only create connectors on client-side to avoid SSR issues
+  // TODO: update when https://github.com/rainbow-me/rainbowkit/issues/2476 is resolved
+  if (typeof window === "undefined") {
+    return [];
+  }
+
+  return connectorsForWallets(
+    [
+      {
+        groupName: "Supported Wallets",
+        wallets: [
+          rainbowWallet,
+          walletConnectWallet,
+          rabbyWallet,
+          metaMaskWallet,
+          injectedWallet,
+        ],
+      },
+    ],
+
+    {
+      appName: "zkFilebox",
+      projectId: "YOUR_PROJECT_ID",
+    }
+  );
+};
+
+const config = createConfig({
   chains: [sepolia],
+  connectors: wagmiConnectors(),
+  transports: {
+    [sepolia.id]: http(),
+  },
   ssr: true,
 });
+
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
